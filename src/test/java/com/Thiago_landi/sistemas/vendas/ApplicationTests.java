34d5fc9f03110ab1d68dc773c8b919963bd9bfc9
Thiago-Landi <thiagolandi0902@gmail.com>
2025-05-26 15:53:58 -0300

    Project created

---
+++ src/test/java/com/Thiago_landi/sistemas/vendas/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Thiago_landi.sistemas.vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
