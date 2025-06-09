e6c3382c6289ed97e86ad6f6c00c52cd5ac95a32
Thiago-Landi <thiagolandi0902@gmail.com>
2025-06-09 09:30:54 -0300

    [security] SecurityConfig

--- src/main/java/com/Thiago_landi/sistemas/vendas/config/SecurityConfig.java
+++ src/main/java/com/Thiago_landi/sistemas/vendas/config/SecurityConfig.java
@@ -5,11 +5,20 @@ import java.util.Map;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.Thiago_landi.sistemas.vendas.security.JwtTokenFilter;
 import com.Thiago_landi.sistemas.vendas.security.JwtTokenProvider;
 
 import lombok.RequiredArgsConstructor;
@@ -33,5 +42,29 @@ public class SecurityConfig {
 		
 		PasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", enconders);
 		return passwordEncoder ;
+	}
+	
+	@Bean
+	AuthenticationManager authenticationManager(AuthenticationConfiguration configuration ) throws Exception {
+		return configuration.getAuthenticationManager();
+	}
+	
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		JwtTokenFilter filter = new JwtTokenFilter(tokenProvider);
+		return http
+				.httpBasic(AbstractHttpConfigurer::disable)
+				.csrf(AbstractHttpConfigurer::disable)
+				.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
+				.sessionManagement(
+						session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authorizeHttpRequests(
+						authorize -> {
+							authorize.requestMatchers("/auth/**").permitAll();
+							authorize.requestMatchers(HttpMethod.POST, "/users/**").permitAll(); // vai ser permitido só porque a criação de um usuario vai ser feita pela propria pessoa que vai usar
+							authorize.anyRequest().authenticated();                                
+						})	
+				.cors(cors -> {})
+				.build();
 	}
 }
